57dfbf45b520a46867da6dab114e2c46027e85b6
Guilherme <guilherme.srocha11@senacsp.edu.br>
2022-08-30 20:17:03 -0300

    Vamo Mano

---
+++ CadastroClientes/Models/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Models
+{
+    public class Cliente
+    {
+        public Cliente(string nome, DateTime nascimento, string email)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            Email = email;
+        }
+
+        public int Id { get; private set; }
+        [Required(ErrorMessage ="NOME É OBRIGATÓRIO!!!")]
+        public string Nome { get; private set; }
+        [Required(ErrorMessage = "NASCIMENTO É OBRIGATÓRIO!!!")]
+        [Display(Name ="Data de Nascimento")]
+        public DateTime Nascimento { get; private set; }
+        [Required(ErrorMessage = "E-MAIL É OBRIGATÓRIO!!!")]
+        public string Email { get; private set;  }
+
+        public int Idade()
+        {
+            int idade = DateTime.Now.Year;
+            if (DateTime.Now.DayOfYear < Nascimento.DayOfYear)
+                idade--;
+            return idade;
+        }
+        public void AtualizarDados (string nome, DateTime nascimento, string email)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            Email = email;
+        }
+    }
+}
