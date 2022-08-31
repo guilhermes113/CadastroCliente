6ae45cf3020b64474d7cd3c009a0e1566f9d03f6
Guilherme <guilherme.srocha11@senacsp.edu.br>
2022-08-30 21:36:50 -0300

    Config Do DbContext

--- CadastroClientes/Startup.cs
+++ CadastroClientes/Startup.cs
@@ -1,7 +1,9 @@
+using CadastroClientes.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,7 @@ namespace CadastroClientes
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CadastroClientes", Version = "v1" });
             });
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MeuDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
