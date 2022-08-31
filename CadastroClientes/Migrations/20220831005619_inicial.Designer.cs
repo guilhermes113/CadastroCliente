f5989fbad22dcb35d117576b38f61c9b8f8b9ee7
Guilherme <guilherme.srocha11@senacsp.edu.br>
2022-08-30 22:03:14 -0300

    Config final

--- CadastroClientes/Migrations/20220831005619_inicial.Designer.cs
+++ CadastroClientes/Migrations/20220831005619_inicial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CadastroClientes.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20220831002216_inicial")]
+    [Migration("20220831005619_inicial")]
     partial class inicial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
